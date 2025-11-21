763a03871874dd93d6f10e853b73f0b04edf930f
L-O-R <lokeshrathi37455@gmail.com>
2025-11-21 13:00:12 +0530

    started to work on the dashboard

--- script/script.js
+++ script/script.js
@@ -1,7 +1,19 @@
-console.log(window.location.href);
-
 document.addEventListener("DOMContentLoaded", () => {
   if (!localStorage.getItem("loggedIn")) {
     window.location.href = "login.html";
+    return;
   }
+
+  const addFormBtn =
+    document.getElementById("add_todo_btn");
+  const logoutBtn = document.getElementById("logout_btn");
+  const addModal = document.getElementById("addModal");
+  const closeBtn = document.getElementById("close_btn");
+
+  addFormBtn.addEventListener("click", () => {
+    addModal.classList.remove("hidden");
+  });
+  closeBtn.addEventListener("click", () => {
+    addModal.classList.add("hidden");
+  });
 });
